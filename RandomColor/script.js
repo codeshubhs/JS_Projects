6cc5939d047dc1b06d6a69d07602ba35a25cdc99
Shubham Yadav <yadavshubham1052003@gmail.com>
2023-06-15 10:31:53 +0530

    first commit

---
+++ RandomColor/script.js
@@ -0,0 +1,15 @@
+const getcolor =()=>{
+    const randomnumber= Math.floor(Math.random()*16777215);
+    //Math.floor(Math.random()*16777215);
+    //console.log(randomnumber);
+    const rendomcode= "#"+randomnumber.toString(16);
+
+ document.body.style.background=rendomcode;
+ document.getElementById("colorcode").innerText=rendomcode;
+navigator.clipboard.writeText(randomcode);
+
+}
+ // event call i can call through document.addEventListener
+document.addEventListener
+("click", 
+getcolor);
